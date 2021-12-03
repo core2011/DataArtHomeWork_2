7826209b61f6618dbdc01a9fe705f2d392ad0c4f
core2011 <56308687+core2011@users.noreply.github.com>
2021-12-03 08:38:57 +0200

    Fix some bug

--- src/com/dataart/homework/HomeWork2.java
+++ src/com/dataart/homework/HomeWork2.java
@@ -12,15 +12,15 @@ import static com.dataart.homework.logic.BusinessLogic.*;
 public class HomeWork2 {
     public static void main(String[] args) {
 
-        ArrayList<Animal> listAnimal = new ArrayList<>();
-
-        listAnimal.add(new Lynx("Ris", "28-09-2020"));
-        listAnimal.add(new Raccoon("Enot", "30-01-2018"));
-        listAnimal.add(new Kangaroo("Kengury", "15-03-2015"));
-        listAnimal.add(new Eagle("Orel", "01-12-2017"));
-        listAnimal.add(new Ostrich("Straus", "23-02-2019"));
-        listAnimal.add(new Carp("Carp", "12-08-2021"));
-        listAnimal.add(new Ram("Taran", "18-06-2020"));
+        ArrayList<Animal> listAnimal = new ArrayList<Animal>(){{
+                add(new Lynx("Ris", "28-09-2020"));
+                add(new Raccoon("Enot", "30-01-2018"));
+                add(new Kangaroo("Kengury", "15-03-2015"));
+                add(new Eagle("Orel", "01-12-2017"));
+                add(new Ostrich("Straus", "23-02-2019"));
+                add(new Carp("Carp", "12-08-2021"));
+                add(new Ram("Taran", "18-06-2020"));
+            }};
 
         printAllProperty(listAnimal);
         callEat(listAnimal);
